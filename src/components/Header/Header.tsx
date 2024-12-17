cc81cee97da50dbd589b5e9e39a2dca0495b854b
Junior Conquista <juniorconquista@Administradors-MacBook-Pro.local>
2024-12-16 21:12:00 -0300

    feat: create home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box } from "@rarui-react/components";
+import { Box, Toggle, Icon } from "@rarui-react/components";
+import { LightbulbFilledIcon, LightbulbOutlinedIcon } from "@rarui/icons";
 import Logo from "@assets/logo-black.svg?react";
 
 const Header: React.FC = () => (
@@ -12,8 +13,25 @@ const Header: React.FC = () => (
     paddingX="$md"
     alignItems="center"
     display="flex"
+    justifyContent="space-between"
   >
     <Logo width="150px" />
+    <Toggle
+      name="toggle-dark-theme"
+      aria-live="polite"
+      readOnly
+      icons={{
+        unchecked: (
+          <Icon
+            source={<LightbulbOutlinedIcon size="small" />}
+            color="$invert"
+          />
+        ),
+        checked: (
+          <Icon source={<LightbulbFilledIcon size="small" />} color="$brand" />
+        ),
+      }}
+    />
   </Box>
 );
 
