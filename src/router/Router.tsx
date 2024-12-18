366356b1772d1bc36479442b93086426d7e3b581
Junior Conquista <juniorconquista@Administradors-MacBook-Pro.local>
2024-12-18 17:10:50 -0300

    chore: adjustando redirect

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { ProxyRoute } from "./proxies";
 
 const Router: React.FC = () => (
-  <BrowserRouter basename="/valorize">
+  <HashRouter>
     <ProxyRoute />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Router;
